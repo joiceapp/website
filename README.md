2b34a2c6e7f8daa9018bfc9a8898b62fd152535c
Marvin Fette <fettema87760@th-nuernberg.de>
2021-11-24 20:11:10 +0100

    First commit start with new age theme

---
+++ README.md
@@ -0,0 +1,12 @@
+# joiceapp.com official website
+
+## Scripts
+- npm run build - build js, scss, pug assets
+- npm run start - start on localhost
+
+
+## Sources runs with
+- https://pugjs.org/api/getting-started.html
+- https://sass-lang.com/
+- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script
+- https://startbootstrap.com/theme/new-age
---
+++ public/js/scripts.js
@@ -0,0 +1,34 @@
+/*!
+* Start Bootstrap - joice: meet and eat v0.0.1 (undefined)
+* Copyright 2013-2021 Start Bootstrap & joiceapp team
+* Licensed under undefined (https://github.com/StartBootstrap/joiceapp-webiste/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
